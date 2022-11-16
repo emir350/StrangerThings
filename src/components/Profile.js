adf21ac92648ef829809af3fb437d85b8adf57e6
emirt <emirtaletovic22@gmail.com>
2022-11-16 16:58:23 -0600

    Final

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,17 +7,34 @@ const Profile = () => {
     
     console.log(profileData.username);
    
+    async function deletePost(id) {
+    const response = await fetch(`https://strangers-things.herokuapp.com/api/2209-FTB-MT-WEB-FT/posts/${id}`, {
+  method: "DELETE",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem("token")}`
+  }
+})
+    const data = await response.json()
+    console.log(data)
+    }
+
     return (
         <div>
             <h1><u>Welcome!</u></h1>
             <h3>{profileData?.username}</h3>
             {
                 profileData.posts ? profileData.posts.map((indivPost, idx) => {
-                    console.log("I am the individual post from the map: ", indivPost)
-                    return <div key={idx}>
+                    if (indivPost.active){
+                        return <div key={idx}>
                         <h2>{indivPost.title}</h2>
                         <Link to={`/profile/${indivPost._id}`}>See Detailed View</Link>
+                        <button onClick={()=>deletePost(indivPost._id)}> Delete </button>
                         </div>
+                    }   else {
+                        return null;
+                    }
+        
                         }) : null
                 }
              </div>
